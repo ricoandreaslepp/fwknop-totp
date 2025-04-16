b91508c8defe340a3759509f052f80b7acf25c43
ricoandreaslepp <ricoandreaslepp@gmail.com>
2025-04-17 02:03:21 +0300

    todo for client TOTP input

--- client/getpasswd.c
+++ client/getpasswd.c
@@ -110,6 +110,8 @@ read_passwd_from_stream(FILE *stream)
     return password;
 }
 
+// TODO: get TOTP from user, basicaly just need to specify the format
+
 /**
  * @brief Function for accepting password input from users
  *
