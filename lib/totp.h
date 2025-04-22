81d2267adaf775c25c69951e830636656670db0c
ricoandreaslepp <ricoandreaslepp@gmail.com>
2025-04-22 18:02:16 +0300

    bump TOTP to 6 digits

--- lib/totp.h
+++ lib/totp.h
@@ -1,6 +1,6 @@
 // libfko imports
 #include "fko.h"
-#include "hmac.h"
+#include "hmac.h" // already includes "digest.h"
 
 // others
 #include <math.h>
@@ -12,6 +12,6 @@
 #define SECRET_LEN 20
 #define HMAC_LENGTH 20
 #define TIME_LEN 8
-#define DIGITS 8 // OTP digits
+#define DIGITS 6 // OTP digits
 #define X 30 // default time step
 #define T0 0 // default value
